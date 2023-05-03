d4b34ce499ba30b4dbb64ed439606b42ec13b0d5
marinasegala <marinasegala01@gmail.com>
2023-05-03 22:26:07 +0200

    okay

--- lib/home.dart
+++ lib/home.dart
@@ -3,7 +3,6 @@ import 'create_travel.dart';
 
 import 'package:planup/db/travel_db.dart';
 import 'package:planup/model/travel.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -14,26 +13,26 @@ class Home extends StatefulWidget {
 
 class _HomePage extends State<Home> {
 
-  late List<Travel> travel;
-  bool isLoading = false;
+  // late List<Travel> travel;
+  // bool isLoading = false;
 
-  @override
-  void initState(){
-    super.initState();
-    refreshTravel();
-  }
+  // @override
+  // void initState(){
+  //   super.initState();
+  //   refreshTravel();
+  // }
 
-  @override
-  void dispose(){
-    TravelDatabase.instance.close();
-    super.dispose();
-  }
+  // @override
+  // void dispose(){
+  //   TravelDatabase.instance.close();
+  //   super.dispose();
+  // }
 
-  Future refreshTravel() async{
-    setState(() => isLoading = true);
-    this.travel = await TravelDatabase.instance.readAll();
-    setState(() => isLoading = false);
-  }
+  // Future refreshTravel() async{
+  //   setState(() => isLoading = true);
+  //   this.travel = await TravelDatabase.instance.readAll();
+  //   setState(() => isLoading = false);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -42,35 +41,36 @@ class _HomePage extends State<Home> {
         title: const Text('I tuoi viaggi'),
         actions: [Icon(Icons.search), SizedBox(width: 12)],
       ),
-      // body: const Center(child: Text('Press the + button below!')),
-      body: Center(
-        child: isLoading
-          ? CircularProgressIndicator()
-          : travel.isEmpty
-          ? Text('No Travel', style: TextStyle(color: Colors.white, fontSize: 24))
-          : buildTravel(),
-        ),
+      body: const Center(child: Text('Press the + button below!')),
+      // body: Center(
+      //   child: isLoading
+      //     ? CircularProgressIndicator()
+      //     : travel.isEmpty
+      //     ? Text('No Travel', style: TextStyle(color: Colors.white, fontSize: 24))
+      //     : buildTravel(),
+      //   ),
       
       // // An example of the floating action button.
       // //
       // // https://m3.material.io/components/floating-action-button/specs
 
-      // floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         
-      //   onPressed: () {
-      //     // Add your onPressed code here!
-      //     // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      //     //                 content: Text('Text button is pressed')));
-      //     // const New();
-      //     Navigator.push(
-      //       context,
-      //       MaterialPageRoute(builder: (context) => const CreateTravelPage()),
-      //     );
-      //   },
-      //   backgroundColor: const Color.fromARGB(255, 255, 217, 104),
-      //   foregroundColor: Colors.black,
-      //   child: const Icon(Icons.add),
-      // ),
+        onPressed: () {
+          // Add your onPressed code here!
+          // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          //                 content: Text('Text button is pressed')));
+          // const New();
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const CreateTravelPage()),
+          );
+        },
+        backgroundColor: const Color.fromARGB(255, 255, 217, 104),
+        foregroundColor: Colors.black,
+        child: const Icon(Icons.add),
+      ),
+      /*
         floatingActionButton: FloatingActionButton(
           onPressed: () async{
             await Navigator.of(context).push(
@@ -80,10 +80,10 @@ class _HomePage extends State<Home> {
           },
           backgroundColor: Colors.black,
           child: Icon(Icons.add),
-        ),
+        ),*/
     );
   }
-
+/*
   Widget buildTravel() => StaggeredGridView.countBuilder(
     padding: EdgeInsets.all(8),
     itemCount: travel.length,
@@ -102,5 +102,5 @@ class _HomePage extends State<Home> {
       )
     },
   );
-  
+  */
 }
