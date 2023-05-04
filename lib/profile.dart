5e66e803c73a18ab104950a2da10fc2969d08f57
GaiaPizzutiUniTn <gaia.pizzuti@studenti.unitn.it>
2023-05-04 19:46:11 +0200

    carousel

--- lib/profile.dart
+++ lib/profile.dart
@@ -16,39 +16,42 @@ class _ProfilePageState extends State<ProfilePage> {
         title: const Text("Il mio profilo"),
       ),
       body: Align(
-          alignment: Alignment.topCenter, //aligns to topCenter
-          child: Column(
-            children: [
-              Container(
-                  padding: const EdgeInsets.fromLTRB(0, 31, 0, 10),
-                  child: const CircleAvatar(
-                    radius: 60,
-                    backgroundImage: AssetImage('assets/profile.jpg'),
-                  )),
-              const Text("Mario", style: TextStyle(fontSize: 25)),
-              const Text("mario_rossi", style: TextStyle(fontSize: 15)),
-              ImageSlideshow(
-                width: 300,
-                height: 200,
-                initialPage: 0, // The page to show when first creating the [ImageSlideshow].
-                indicatorColor: Colors.blue,
-                indicatorRadius: 3,
-                indicatorBackgroundColor: Colors.grey,
-                autoPlayInterval: 3000,
-                isLoop: true,
-                // The widgets to display in the [ImageSlideshow], add the sample image file into the images folder
-                children: [
-                  Image.asset(
-                    'images/sample_image_1.jpg',
-                    fit: BoxFit.cover,
-                  ),
-                  Image.asset(
-                    'images/sample_image_2.jpg',
-                    fit: BoxFit.cover,
-                  ),
-                ],
-              ),
-          ],)),
+        alignment: Alignment.topCenter, //aligns to topCenter
+        child: Column(
+          children: [
+            Container(
+                padding: const EdgeInsets.fromLTRB(0, 31, 0, 10),
+                child: const CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/profile.jpg'),
+                )),
+            const Text("Mario", style: TextStyle(fontSize: 25)),
+            const Text("mario_rossi", style: TextStyle(fontSize: 15)),
+            ImageSlideshow(
+              width: 300,
+              height: 200,
+              initialPage:
+                  0, // The page to show when first creating the [ImageSlideshow].
+              indicatorColor: Colors.blue,
+              indicatorRadius: 3,
+              indicatorBackgroundColor: Colors.grey,
+              autoPlayInterval: 3000,
+              isLoop: true,
+              // The widgets to display in the [ImageSlideshow], add the sample image file into the images folder
+              children: [
+                Image.asset(
+                  'images/sample_image_1.jpg',
+                  fit: BoxFit.cover,
+                ),
+                Image.asset(
+                  'images/sample_image_2.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
