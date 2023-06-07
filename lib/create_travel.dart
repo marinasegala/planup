26a17737cd61404b2c71d4aef5fbe6e8e0cccceb
marinasegala <marinasegala01@gmail.com>
2023-06-07 11:19:17 +0200

    change date

--- lib/create_travel.dart
+++ lib/create_travel.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crea_radio_button/crea_radio_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -341,36 +342,113 @@ class _CreateTravelFormState extends State<CreateTravelPage> {
         ]),
       );
     }
+    List<RadioOption> options = [
+      RadioOption("Giornata", AppLocalizations.of(context)!.oneDay),
+      RadioOption("Weekend", AppLocalizations.of(context)!.weekend),
+      RadioOption("Settimana", AppLocalizations.of(context)!.week),
+      RadioOption("Altro", AppLocalizations.of(context)!.other),
+    ];
 
     var macroCharts = buildCalendarDialogButton();
+    // var microCharts = Center(
+    //     child: ToggleSwitch(
+    //   initialLabelIndex: 0,
+    //   // minWidth: MediaQuery.of(context).size.width * 0.225,
+    //   // minHeight: MediaQuery.of(context).size.height * 0.06,
+    //   activeBgColor: const [Color.fromARGB(255, 59, 94, 115)],
+    //   inactiveBgColor: const Color.fromARGB(255, 223, 227, 229),
+    //   totalSwitches: 4,
+    //   labels: [
+    //     AppLocalizations.of(context)!.oneDay,
+    //     AppLocalizations.of(context)!.weekend,
+    //     AppLocalizations.of(context)!.week,
+    //     AppLocalizations.of(context)!.other
+    //   ],
+    //   onToggle: (index) {
+    //     switch (index) {
+    //       case 1:
+    //         date = 'Weekend';
+    //         break;
+    //       case 2:
+    //         date = 'Settimana';
+    //         break;
+    //       case 3:
+    //         date = 'Altro';
+    //         break;
+    //     }
+    //   },
+    // ));
     var microCharts = Center(
-        child: ToggleSwitch(
-      initialLabelIndex: 0,
-      minWidth: MediaQuery.of(context).size.width * 0.225,
-      minHeight: MediaQuery.of(context).size.height * 0.06,
-      activeBgColor: const [Color.fromARGB(255, 59, 94, 115)],
-      inactiveBgColor: const Color.fromARGB(255, 223, 227, 229),
-      totalSwitches: 4,
-      labels: [
-        AppLocalizations.of(context)!.oneDay,
-        AppLocalizations.of(context)!.weekend,
-        AppLocalizations.of(context)!.week,
-        AppLocalizations.of(context)!.other
-      ],
-      onToggle: (index) {
-        switch (index) {
-          case 1:
-            date = 'Weekend';
-            break;
-          case 2:
-            date = 'Settimana';
-            break;
-          case 3:
-            date = 'Altro';
-            break;
-        }
-      },
-    ));
+      child: RadioButtonGroup(
+        options: options,
+        preSelectedIdx: 0,
+        spaceBetween: 12.0,
+        betweenMultiLines: 12.0,
+        vertical: false,
+        multilineNumber: 3,
+        textStyle: const TextStyle(
+            fontSize: 15,
+            color: Colors.black),
+        mainAxisAlignment:
+            MainAxisAlignment.center,
+        crossAxisAlignment:
+            CrossAxisAlignment.center,
+        selectedColor:
+            const Color.fromARGB(
+                255, 195, 190, 190),
+        mainColor: const Color.fromARGB(
+            255, 195, 190, 190),
+        selectedBorderSide:
+            const BorderSide(
+                width: 2,
+                color: Color.fromARGB(
+                    255, 64, 137, 168)),
+        buttonWidth: MediaQuery
+                    .of(context)
+                .size
+                .width *
+            0.4,
+        buttonHeight:
+            MediaQuery.of(context)
+                    .size
+                    .height *
+                0.05,
+        callback: (RadioOption val) {
+          setState(() {
+            date = val.label;
+          });
+    }),);
+        // child: ToggleSwitch(
+        //   initialLabelIndex: 0,
+        //   minWidth: 85.0,
+        //   minHeight: 50.0,
+        //   activeBgColor: const [Color.fromARGB(255, 59, 94, 115)],
+        //   inactiveBgColor: const Color.fromARGB(255, 223, 227, 229),
+        //   totalSwitches: 4,
+        //   labels: [
+        //     AppLocalizations.of(context)!.oneDay,
+        //     AppLocalizations.of(context)!.weekend,
+        //     AppLocalizations.of(context)!.week,
+        //     AppLocalizations.of(context)!.other
+        //   ],
+        //   onToggle: (index) {
+            // switch (index) {
+            //   case 1:
+            //     date = 'Weekend';
+            //     break;
+            //   case 2:
+            //     date = 'Settimana';
+            //     break;
+            //   case 3:
+            //     date = 'Altro';
+            //     break;
+            // }
+            // setState(() {
+            //   _swapDate = !_swapDate;
+            // });
+          //   print('switched to $index -- $date');
+          // },
+    
 
     var swapTile = Container(
       child: (_swapDate) ? macroCharts : microCharts,
